25a603dc315ba3ef173f9bc0f9fc0905ac621550
leedongyeop <ldy_1204@naver.com>
2022-12-16 13:48:14 +0900

    Consumer 사용 예제

---
+++ src/chap03/functionalInterfaceExample/ConsumerExample.java
@@ -0,0 +1,21 @@
+package chap03.functionalInterfaceExample;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerExample<T> {
+
+    public static <T> void forEach(List<T> list, Consumer<T> c) {
+        for (T t : list) {
+            c.accept(t);
+        }
+    }
+
+    public static void main(String[] args) {
+        forEach(
+                Arrays.asList(1, 2, 3, 4, 5),
+                (Integer i) -> System.out.println(i)
+        );
+    }
+}
