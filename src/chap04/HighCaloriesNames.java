bcb3ba281e872364bd9f9631bb14d5349dfc1f1d
leedongyeop <ldy_1204@naver.com>
2023-01-24 20:56:56 +0900

    feat: 4장 스트림 소개 예제 일부

---
+++ src/chap04/HighCaloriesNames.java
@@ -0,0 +1,35 @@
+package chap04;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class HighCaloriesNames {
+    public static final List<Dish> menu = Arrays.asList(
+            new Dish("pork", false, 800, Dish.Type.MEAT),
+            new Dish("beef", false, 700, Dish.Type.MEAT),
+            new Dish("chicken", false, 400, Dish.Type.MEAT),
+            new Dish("french fries", true, 530, Dish.Type.OTHER),
+            new Dish("rice", true, 350, Dish.Type.OTHER),
+            new Dish("season fruit", true, 120, Dish.Type.OTHER),
+            new Dish("pizza", true, 550, Dish.Type.OTHER),
+            new Dish("prawns", false, 400, Dish.Type.FISH),
+            new Dish("salmon", false, 450, Dish.Type.FISH)
+    );
+
+    public static void main(String[] args) {
+        List<String> names = menu.stream()
+                .filter(dish -> {
+                    System.out.println("filtering " + dish.getName());
+                    return dish.getCalories() > 300;
+                })
+                .map(dish -> {
+                    System.out.println("mapping " + dish.getName());
+                    return dish.getName();
+                })
+                .limit(3)
+                .collect(toList());
+        System.out.println(names);
+    }
+}
